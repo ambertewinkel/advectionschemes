f0ae8bcaef1986c3d8d12eb806bd64faeb0fc080
ambertewinkel <ambertewinkel@gmail.com>
2023-11-30 16:40:15 +0000

    Coded up and plotted basic schemes

--- main.py
+++ main.py
@@ -58,13 +58,13 @@ def main():
     
     # Initial conditions
     a, b = 0.1, 0.5
-    C = 0.4                     # Courant number
+    c = 0.4                     # Courant number
     nx = 40                     # number of points in space
-    nt = 10                     # number of time steps
+    nt = 100                     # number of time steps
     x = np.linspace(0, 1, nx+1) # points in space
     dx = x[1] - x[0]            # length of spatial step
     dt = 0.1                    # time step
-    u = C*dx/dt                 # velocity
+    u = c*dx/dt                 # velocity
 
     # Calculate initial functions
     psi1_in = initial1(x, a, b)
@@ -78,40 +78,88 @@ def main():
     #### Schemes ####
     #################
 
+    # FTBS
+    psi1_ftbs = sch.ftbs(psi1_in.copy(), nt, c) 
+    psi2_ftbs = sch.ftbs(psi2_in.copy(), nt, c) 
+
+    # FTFS
+    psi1_ftfs = sch.ftfs(psi1_in.copy(), nt, c) 
+    psi2_ftfs = sch.ftfs(psi2_in.copy(), nt, c) 
+
+    # FTCS
+    psi1_ftcs = sch.ftcs(psi1_in.copy(), nt, c) 
+    psi2_ftcs = sch.ftcs(psi2_in.copy(), nt, c) 
+
+    # CTBS
+    psi1_ctbs = sch.ctbs(psi1_in.copy(), nt, c) 
+    psi2_ctbs = sch.ctbs(psi2_in.copy(), nt, c) 
+
+    # CTFS
+    psi1_ctfs = sch.ctfs(psi1_in.copy(), nt, c) 
+    psi2_ctfs = sch.ctfs(psi2_in.copy(), nt, c) 
+
     # CTCS
-    psi1_ctcs = sch.ctcs(psi1_in.copy(), nt, C) 
-    psi2_ctcs = sch.ctcs(psi2_in.copy(), nt, C) 
+    psi1_ctcs = sch.ctcs(psi1_in.copy(), nt, c) 
+    psi2_ctcs = sch.ctcs(psi2_in.copy(), nt, c) 
 
     # MPDATA
-    psi1_mpdata = sch.mpdata(psi1_in.copy(), nt, C)
-    psi2_mpdata = sch.mpdata(psi2_in.copy(), nt, C)
+    psi1_mpdata = sch.mpdata(psi1_in.copy(), nt, c)
+    psi2_mpdata = sch.mpdata(psi2_in.copy(), nt, c)
 
     ##########################
     #### Plotting schemes ####
     ##########################
     
     plt.plot(x, psi1_an, label='Analytic', linestyle='-', color='k')
+    plt.plot(x, psi1_ftbs, label='FTBS')
+    plt.plot(x, psi1_ftfs, label='FTFS')
+    plt.plot(x, psi1_ftcs, label='FTCS')
+    plt.plot(x, psi1_ctbs, label='CTBS')
+    plt.plot(x, psi1_ctfs, label='CTFS')
     plt.plot(x, psi1_ctcs, label='CTCS')
+    plt.ylim(-0.5, 1.5)
+    plt.xlabel('x')
+    plt.ylabel('$\Psi_1$')
+    plt.title(f'$\Psi_1$ at t={nt*dt} - Basic Schemes - c = {c}')
+    plt.legend()
+    plt.savefig('Psi1_bs.jpg')
+    plt.show()
+
+    plt.plot(x, psi1_an, label='Analytic', linestyle='-', color='k')
     plt.plot(x, psi1_mpdata, label='MPDATA')
     plt.ylim(-0.5, 1.5)
     plt.xlabel('x')
     plt.ylabel('$\Psi_1$')
-    plt.title('$\Psi_1$ at t=1.0')
+    plt.title(f'$\Psi_1$ at t={nt*dt} - Alternative Schemes - c = {c}')
     plt.legend()
-    plt.savefig('Psi1.jpg')
+    plt.savefig('Psi1_as.jpg')
     plt.show()
 
     plt.plot(x, psi2_an, label='Analytic', linestyle='-', color='k')
+    plt.plot(x, psi2_ftbs, label='FTBS')
+    plt.plot(x, psi2_ftfs, label='FTFS')
+    plt.plot(x, psi2_ftcs, label='FTCS')
+    plt.plot(x, psi2_ctbs, label='CTBS')
+    plt.plot(x, psi2_ctfs, label='CTFS')
     plt.plot(x, psi2_ctcs, label='CTCS')
+    plt.ylim(-0.5, 1.5)
+    plt.xlabel('x')
+    plt.ylabel('$\Psi_2$')
+    plt.title(f'$\Psi_2$ at t={nt*dt} - Basic Schemes - c = {c}')
+    plt.legend()
+    plt.savefig('Psi2_bs.jpg')
+    plt.show()
+    
+    plt.plot(x, psi2_an, label='Analytic', linestyle='-', color='k')
     plt.plot(x, psi2_mpdata, label='MPDATA')
     plt.ylim(-0.5, 1.5)
     plt.xlabel('x')
     plt.ylabel('$\Psi_2$')
-    plt.title('$\Psi_2$ at t=1.0')
+    plt.title(f'$\Psi_2$ at t={nt*dt} - Alternative Schemes - c = {c}')
     plt.legend()
-    plt.savefig('Psi2.jpg')
+    plt.savefig('Psi2_as.jpg')
     plt.show()
-    
+
     #####################
     #### Experiments ####
     #####################
--- schemes.py
+++ schemes.py
@@ -1,6 +1,144 @@
 # Module file with various schemes to analyze in main.py
+# Schemes included: FTBS, FTFS, FTCS, CTBS, CTFS, CTCS, MPDATA
 
 import numpy as np
+
+def ftbs(init, nt, c):
+    """
+    This function computes the FTBS (forward in time, backward in space)
+    finite difference scheme for an initial field, number of time steps nt
+    with length dt, and a given Courant number. A periodic spatial domain 
+    is assumed.
+    --- Input ---
+    init    : array of floats, initial field to advect
+    nt      : integer, total number of time steps to take
+    c       : Courant number. c = u*dt/dx where u is the velocity and dx 
+            is the spatial discretisation
+    --- Output --- 
+    field   : 1D array of floats. Outputs the final timestep after advecting 
+            the initial condition. Dimensions: length of init.
+    """
+
+    # Setup and initial condition
+    field = init
+
+    # Time stepping
+    for it in range(1, nt):
+        field = field - c*(field - np.roll(field,1))
+
+    return field
+
+def ftfs(init, nt, c):
+    """
+    This function computes the FTFS (forward in time, forward in space)
+    finite difference scheme for an initial field, number of time steps nt
+    with length dt, and a given Courant number. A periodic spatial domain 
+    is assumed.
+    --- Input ---
+    init    : array of floats, initial field to advect
+    nt      : integer, total number of time steps to take
+    c       : Courant number. c = u*dt/dx where u is the velocity and dx 
+            is the spatial discretisation
+    --- Output --- 
+    field   : 1D array of floats. Outputs the final timestep after advecting 
+            the initial condition. Dimensions: length of init.
+    """
+
+    # Setup and initial condition
+    field = init
+
+    # Time stepping
+    for it in range(1, nt):
+        field = field - c*(np.roll(field,-1) - field)
+
+    return field
+
+def ftcs(init, nt, c):
+    """
+    This function computes the FTCS (forward in time, centered in space)
+    finite difference scheme for an initial field, number of time steps nt
+    with length dt, and a given Courant number. A periodic spatial domain 
+    is assumed.
+    --- Input ---
+    init    : array of floats, initial field to advect
+    nt      : integer, total number of time steps to take
+    c       : Courant number. c = u*dt/dx where u is the velocity and dx 
+            is the spatial discretisation
+    --- Output --- 
+    field   : 1D array of floats. Outputs the final timestep after advecting 
+            the initial condition. Dimensions: length of init.
+    """
+
+    # Setup and initial condition
+    field = init
+
+    # Time stepping
+    for it in range(1, nt):
+        field = field - 0.5*c*(np.roll(field,-1) - np.roll(field,1))
+
+    return field
+
+def ctbs(init, nt, c):
+    """
+    This function computes the CTBS (centered in time, backward in space)
+    finite difference scheme for an initial field, number of time steps nt
+    with length dt, and a given Courant number. A periodic spatial domain 
+    is assumed.
+    --- Input ---
+    init    : array of floats, initial field to advect
+    nt      : integer, total number of time steps to take
+    c       : Courant number. c = u*dt/dx where u is the velocity and dx 
+            is the spatial discretisation
+    --- Output --- 
+    field   : 1D array of floats. Outputs the final timestep after advecting 
+            the initial condition. Dimensions: length of init.
+    """
+
+    # Setup and initial condition
+    field = np.zeros(len(init))
+    field_old = init
+
+    # First time step is forward in time, backward in space (FTBS)
+    field = field_old - c*(field_old - np.roll(field_old,1))
+
+    # Time stepping
+    for it in range(1, nt):
+        field_new = field_old - 2*c*(field - np.roll(field,1))
+        field_old = field
+        field = field_new
+        
+    return field
+
+def ctfs(init, nt, c):
+    """
+    This function computes the CTFS (centered in time, forward in space)
+    finite difference scheme for an initial field, number of time steps nt
+    with length dt, and a given Courant number. A periodic spatial domain 
+    is assumed.
+    --- Input ---
+    init    : array of floats, initial field to advect
+    nt      : integer, total number of time steps to take
+    c       : Courant number. c = u*dt/dx where u is the velocity and dx 
+            is the spatial discretisation
+    --- Output --- 
+    field   : 1D array of floats. Outputs the final timestep after advecting 
+            the initial condition. Dimensions: length of init.
+    """
+
+    # Setup and initial condition
+    field = np.zeros(len(init))
+    field_old = init
+
+    # First time step is forward in time, forward in space (FTFS)
+    field = field_old - c*(np.roll(field_old,-1) - field_old)
+
+    # Time stepping
+    for it in range(1, nt):
+        field_new = field_old - 2*c*(np.roll(field,-1) - field)
+        field_old = field
+        field = field_new
+        
+    return field
 
 def ctcs(init, nt, c):
     """
@@ -15,7 +153,7 @@ def ctcs(init, nt, c):
             is the spatial discretisation
     --- Output --- 
     field   : 1D array of floats. Outputs the final timestep after advecting 
-            the initial condition. Dimensions: (nt+1) x length of init.
+            the initial condition. Dimensions: length of init.
     """
 
     # Setup and initial condition
@@ -25,13 +163,34 @@ def ctcs(init, nt, c):
     # First time step is forward in time, centered in space (FTCS)
     field = field_old - 0.5*c*(np.roll(field_old,-1) - np.roll(field_old,1))
 
-    # Time stepping with CTCS
+    # Time stepping
     for it in range(1, nt):
         field_new = field_old - c*(np.roll(field,-1) - np.roll(field,1))
         field_old = field
         field = field_new
         
     return field
+
+def upwind():
+    print()
+
+def artdiff():
+    print()
+
+def semiLag():
+    print()
+
+def btbs(): #implicit
+    print()
+
+def btcs(): #implicit
+    print()
+
+def cnbs(): #implicit
+    print()
+
+def cncs(): #implicit
+    print()
 
 def mpdata(init, nt, c, eps=1e-6):
     """
